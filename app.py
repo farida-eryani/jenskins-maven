2aeb0db7d7c3f135f8224e8eab1a85a8275d68ee
Ubuntu <ubuntu@ip-172-31-44-176.ec2.internal>
2024-03-16 14:23:33 +0000

    my second commit

--- app.py
+++ app.py
@@ -1,29 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(name)
 
 @app.route('/')
 def hello():
-    return """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Hello World</title>
-        <style>
-            body {
-                color: blue;
-                font-size: 48px;
-                font-family: Arial, sans-serif;
-                text-align: center;
-                margin-top: 20%;
-            }
-        </style>
-    </head>
-    <body>
-        <p>Hello World from Docker!</p>
-    </body>
-    </html>
-    """
+    # Show the world symple
+    return 'Hello World from Docker!'
 
-if __name__ == '__main__':
+if name == 'main':
     app.run(host='0.0.0.0', port=80)
-
